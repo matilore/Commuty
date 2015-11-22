809566622cd90554813289ec83117de3240a5593
Matias Lorenzo <matiaslorenzo@hotmail.it>
2015-11-22 19:39:50 +0100

    post controller updated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,8 +20,34 @@ class PostsController < ApplicationController
 			redirect_to posts_path
 		else
 			flash[:error] = "The post could not be added"
+			redirect_to new_post_path
 		end
 	end
+
+	def edit
+		post_id = params[:id]
+		@post = Post.find_by(id: post_id)
+		
+	end
+
+	def update
+		post_id = params[:id]
+		@post = Post.find_by(id: post_id)
+		if @post.update(post_params)
+			redirect_to post_path(@post.id)
+		else
+			flash[:error] = "The post could not be updated"
+			redirect_to edit_post_path
+		end		
+	end
+
+	def destroy
+		post_id = params[:id]
+		@post = Post.find_by(post_id)
+		@post.destroy
+		redirect_to posts_path
+	end
+
 
 	private
 	def post_params
