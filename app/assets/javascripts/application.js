b131baf0cc0fcc202623ad8a7ebcbedf491e644d
Matias Lorenzo <matiaslorenzo@hotmail.it>
2015-11-30 00:55:56 +0100

    comments jquery added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,8 +18,75 @@
 //= require_tree .
 
 
+function createParClass() {
+		var arrayParagraphs = $('.main-content p')
+		for (var i = 0; i < arrayParagraphs.length; i++) {
+			var idName = 'par'+ i;
+			$(arrayParagraphs[i]).attr('id', idName);
+			$(arrayParagraphs[i]).append("<span style='width:6px;'class='glyphicon glyphicon-pencil pull-right hide' aria-hidden='true'></span><span style='width:6px; 'class='glyphicon glyphicon-sunglasses pull-right hide' aria-hidden='true'></span>");
+			//inserire qui un pulsante per aggiungere commento con identificatore tag
+
+		};
+	}
+
+	function showComments() {
+		$('.main-content p span.glyphicon-sunglasses').click(function(event){
+			var pId = $(event.target).parent().attr('id');
+			console.log(pId)
+
+		if (!($(event.target).hasClass('commentShowing'))) {		
+			var comment = $('<p>This is a comment</p>').addClass(pId);
+			$('.comment').append(comment);
+			$(event.target).toggleClass('commentShowing')
+		} else {
+			$('.comment').children("." + pId).remove();
+			$(event.target).toggleClass('commentShowing')
+			}
+		});
+	}
+
+	function writeComments() {
+		$('.main-content p span.glyphicon-pencil').click(function(){
+			$('.form-group-comment').toggleClass('hide');
+		})
+	}
+
+	function commentMode() {
+		$('.comment-mode').click(function(){
+			if ($('.main-content p span').hasClass('hide')) {
+				$(this).text('Read Mode');
+				$('.main-content p span').toggleClass('hide');
+			} else {
+				$(this).text('Comment Mode');
+				$('.main-content p span').toggleClass('hide');
+			}
+			
+		});
+
+		
+	}
+
+	function createPopups() {
+		var spanAction = ['Write a comment', 'Read a comment'];
+		$('.main-content p span').mouseover(function(event){
+			if ($(event.target).hasClass('glyphicon-pencil')) {
+				$(event.target).attr('title', spanAction[0]);
+			} else {
+				$(event.target).attr('title', spanAction[1]);
+			}
+		});
+	}
+
+
 $(document).ready(function(){
 	$('.diffs-btn').click(function() {
-		var modal = $('.diffs-modal').modal('show')
-	})
+		var modal = $('.diffs-modal').modal('show');
+	});
+
+	createParClass();
+	showComments();
+	writeComments();
+	commentMode();
+	createPopups();
+	
 });
